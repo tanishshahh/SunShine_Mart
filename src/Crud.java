e9a67e069275904275a1fed74b3ac83413146407
TANISH <shahtanish18@gamil.com>
2026-03-11 12:01:58 +0530

    Customer crud created

--- src/Crud.java
+++ src/Crud.java
@@ -33,6 +33,7 @@ public class Crud {
 
         ps.executeUpdate();
         System.out.println("Product Updated");
+        con.close();
     }
 
     public void deleteProduct(int id) throws Exception {
@@ -42,6 +43,7 @@ public class Crud {
         ps.executeUpdate();
 
         System.out.println("Product Deleted");
+        con.close();
     }
 
     public void viewProducts() throws Exception {
@@ -57,7 +59,9 @@ public class Crud {
             System.out.println("Product Quantity: " + rs.getInt(5));
             System.out.println("Product Vendor Id:"+ rs.getInt(6));
             System.out.println("---------------------------");
+
         }
+        con.close();
     }
 
     public void searchProduct(int id) throws Exception {
@@ -70,6 +74,79 @@ public class Crud {
             System.out.println("Product Name: " + rs.getString(2));
             System.out.println("Product Quantity: " + rs.getInt(3));
         }
+        con.close();
+    }
+
+    //===============Customer==================
+    public void insertCustomer(customer c) throws Exception {
+        Connection con = DBConnection.getPostgresConnection();
+        PreparedStatement ps =con.prepareStatement("insert into public.customer values(?,?,?,?,?)");
+
+        ps.setInt(1, c.getCust_id());
+        ps.setString(2, c.getCust_name());
+        ps.setString(3, c.getCust_number());
+        ps.setString(4,c.getCust_address());
+        ps.setString(5,c.getCust_email());
+
+        ps.executeUpdate();
+        System.out.println("Customer Inserted");
+        con.close();
+    }
+
+    public void updateCustomer(customer c) throws Exception {
+        Connection con = DBConnection.getPostgresConnection();
+        PreparedStatement ps =con.prepareStatement("update public.customer set cust_name=?,cust_number=?,cust_address=?,cust_email=? where cust_id=?");
+
+        ps.setString(1, c.getCust_name());
+        ps.setString(2, c.getCust_number());
+        ps.setString(3,c.getCust_address());
+        ps.setString(4,c.getCust_email());
+        ps.setInt(5, c.getCust_id());
+
+        ps.executeUpdate();
+        System.out.println("Customer Updated");
+        con.close();
+    }
+
+    public void deleteCustomer(int id) throws Exception {
+        Connection con = DBConnection.getPostgresConnection();
+        PreparedStatement ps =con.prepareStatement("delete from public.customer where cust_id=?");
+        ps.setInt(1, id);
+        ps.executeUpdate();
+
+        System.out.println("Customer Deleted");
+        con.close();
+    }
+
+    public void viewCustomer() throws Exception {
+        Connection con = DBConnection.getPostgresConnection();
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("select * from public.customer");
+        while (rs.next()) {
+
+            System.out.println("Customer ID: " + rs.getInt(1));
+            System.out.println("Customer Name: " + rs.getString(2));
+            System.out.println("Customer Number: "+rs.getString(3));
+            System.out.println("Customer Address"+rs.getString(4));
+            System.out.println("Customer Email " + rs.getString(5));
+            System.out.println("---------------------------");
+        }
+        con.close();
+    }
+
+    public void searchCustomer(int id) throws Exception {
+        Connection con = DBConnection.getPostgresConnection();
+        PreparedStatement ps =con.prepareStatement("select * from public.customer where cust_id=?");
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            System.out.println("Customer ID: " + rs.getInt(1));
+            System.out.println("Customer Name: " + rs.getString(2));
+            System.out.println("Customer Number: "+rs.getString(3));
+            System.out.println("Customer Address"+rs.getString(4));
+            System.out.println("Customer Email " + rs.getString(5));
+        }
+        con.close();
     }
 
 }
